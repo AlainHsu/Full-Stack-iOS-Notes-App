a241fb526b7ae72b088f6a6878ca49df92dfc540
Alain Hsu <alain.hsu@outlook.com>
2021-06-25 12:02:26 +0800

    feat(server): add fetch, delete and update

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.post("/create", (req, res) => {
     var note = new Data({
         note: req.get("note"),
         title: req.get("title"),
-        date: req.get("data")
+        date: req.get("date")
     })
 
     note.save().then(() => {
@@ -31,19 +31,44 @@ app.post("/create", (req, res) => {
     })
 })
 
-// http://172.24.4.40:8081/create
-var server = app.listen(8081, "172.24.4.40", () => {
-    console.log("Server is running!")
-})
-
 // FETCH ALL NOTES  
 // GET request
-app.get('fetch', (req, res) => {
-
+app.get('/fetch', (req, res) => {
+    Data.find({}).then((DBItems) => {
+        res.send(DBItems)
+    })
 })
 
 // DELATE A NOTE
 // POST request
+app.post('/delete', function (req, res) {
+    Data.findOneAndDelete({
+        _id: req.get("id")
+    }, (err) => {
+        console.log("Failed" + err)
+    })
+    res.send('Deleted ' + req.get("id") + " succeed!")
+})
 
 // UPDATE A NOTE
 // POST request
+app.post("/update", function (req, res) {
+    let params = {
+        note: req.get("note"),
+        title: req.get("title"),
+        date: req.get("date")
+    };
+    for (let prop in params) if (!params[prop]) delete params[prop];
+
+    Data.findOneAndUpdate({
+        _id: req.get("id")
+    }, params, (err) => {
+        console.log("Failed" + err)
+    })
+    res.send('Update ' + req.get("id") + " succeed!")
+})
+
+// http://172.24.4.40:8081/create
+var server = app.listen(8081, "172.24.4.40", () => {
+    console.log("Server is running!")
+})
